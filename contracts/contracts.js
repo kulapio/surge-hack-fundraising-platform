f9be2a116145063ba22f907bf8e35ac5296a3cd3
Nattapon Nimakul <totizy@gmail.com>
2019-05-26 01:14:59 +0700

    add omg token

--- contracts/contracts.js
+++ contracts/contracts.js
@@ -10,23 +10,28 @@ module.exports = {
       // The order here corresponds to the order of `web3.eth.getAccounts`, so the first one is the `defaultAccount`
       /*,accounts: [
         {
-          privateKey: "your_private_key",
+          privateKey: "a0596b7b461f2c16195451dc504fa37cd2ddf17cb8f9e5a6ee4d9e8ad41db9ba", // address: 0xff0B7DC5c86427B3CaC46C2B0A7E4B7a4E673030
           balance: "5 ether"  // You can set the balance of the account in the dev environment
                               // Balances are in Wei, but you can specify the unit with its name
         },
         {
-          privateKeyFile: "path/to/file", // Either a keystore or a list of keys, separated by , or ;
-          password: "passwordForTheKeystore" // Needed to decrypt the keystore file
+          privateKey: "7220522bb5157dd254df843ad77d229edffae9593e9f5b032f4377dbbd06ba57", // address: 0xffe15c7CFD10Ea85e63c499c06080970c1654fC4
+          balance: "5 ether"  // You can set the balance of the account in the dev environment
+                              // Balances are in Wei, but you can specify the unit with its name
         },
-        {
-          mnemonic: "12 word mnemonic",
-          addressIndex: "0", // Optional. The index to start getting the address
-          numAddresses: "1", // Optional. The number of addresses to get
-          hdpath: "m/44'/60'/0'/0/" // Optional. HD derivation path
-        },
-        {
-          "nodeAccounts": true // Uses the Ethereum node's accounts
-        }
+        // {
+        //   privateKeyFile: "path/to/file", // Either a keystore or a list of keys, separated by , or ;
+        //   password: "passwordForTheKeystore" // Needed to decrypt the keystore file
+        // },
+        // {
+        //   mnemonic: "12 word mnemonic",
+        //   addressIndex: "0", // Optional. The index to start getting the address
+        //   numAddresses: "1", // Optional. The number of addresses to get
+        //   hdpath: "m/44'/60'/0'/0/" // Optional. HD derivation path
+        // },
+        // {
+        //   "nodeAccounts": true // Uses the Ethereum node's accounts
+        // }
       ]*/
     },
     // order of connections the dapp should connect to
@@ -57,7 +62,11 @@ module.exports = {
        args: [ 100 ]
       },
       TokenExchange: {
-        args: [ 'OMG', '0xc4375b7de8af5a38a93548eb8453a498222c4ff2', (2.03 * 1e18).toString() ],
+        args: [ 'OMG', '0xc4375b7de8af5a38a93548eb8453a498222c4ff2', (2.03 * 1e18).toString() ]
+      },
+      OMGToken: {
+        args: [ 'OMG', 'OMG', 18 ],
+        // onDeploy: ["OMGToken.methods.transfer('0x5666c33bb922F97B6721D3f932dfD9350C933a6F', \"100000000000000000000000000\").send()"]
       }
     }
   },
